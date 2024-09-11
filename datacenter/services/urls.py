2a0453f19071245437d200a0c30f01ba0a3f37b6
1O$K <74111513+1OSK@users.noreply.github.com>
2024-09-11 10:52:10 +0300

    лаб 1

---
+++ datacenter/services/urls.py
@@ -0,0 +1,9 @@
+# services/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.service_list, name='service_list'),
+    path('service/<int:service_id>/', views.service_detail, name='service_detail'),
+    path('request/<int:request_id>/', views.request_detail, name='request_detail'),
+]
